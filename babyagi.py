58c6a58803a37d52389b4259d425e27b0d8c76c1
saten-private <38902911+saten-private@users.noreply.github.com>
2023-06-26 12:35:17 +0900

    Fixed to create a folder in the middle of the path when writing a file.

--- babyagi.py
+++ babyagi.py
@@ -743,6 +743,7 @@ def analyze_command_result(result: str) -> str:
     return '\n'.join(result_lines)  # If no match, return the last 30 lines
 
 def write_file(file_path: str, content: str):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, "w") as file:
         file.write(content)
 

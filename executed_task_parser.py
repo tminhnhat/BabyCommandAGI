5d8e3a16ff193354eb83e17f1539a1342cfea469
saten-private <38902911+saten-private@users.noreply.github.com>
2023-07-01 10:56:12 +0900

    Correction of incorrect colon position

--- executed_task_parser.py
+++ executed_task_parser.py
@@ -5,7 +5,7 @@ class ExecutedTaskParser:
     def encode(self, input_data: List[dict]) -> str:
         output = ""
         for item in input_data:
-            output += f"{item['type']:} {item['target']}\n"
+            output += f"{item['type']}: {item['target']}\n"
             output += "```\n"
             output += f"{item['result']}\n"
             output += "```\n"

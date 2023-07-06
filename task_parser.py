5d60f98a4a5c63d9f64bc0627c83452017b22753
saten-private <38902911+saten-private@users.noreply.github.com>
2023-07-06 20:11:11 +0900

    - Restore the TaskParser decode result, which was previously List[dict] instead of deque. - Change the order of processing because the decode method was read before the decode method was defined.

--- task_parser.py
+++ task_parser.py
@@ -3,6 +3,75 @@ from collections import deque
 
 class TaskParser:
 
+    def decode(self, input_string: str) -> List[dict]:
+        data = input_string.strip().split("type:")[1:]
+        if len(data) == 0:
+            raise ValueError("No valid items found")
+        parsed_data = []
+
+        for item in data:
+            item = "type:" + item.strip()
+            type, path, content = self._split_data(item)
+            dict = {}
+            dict["type"] = type
+            if path is not None:
+                dict["path"] = path
+            dict["content"] = content
+            parsed_data.append(dict)
+
+        if len(parsed_data) == 0:
+            raise ValueError("No valid items found")
+
+        return parsed_data
+    
+    def _split_data(self, input_data):
+        lines = input_data.split('\n')
+        type_line, path_line = None, None
+        content_lines = []
+        record_content = False
+        has_content = False
+
+        for line in lines:
+            #line = line.strip()
+            if line.startswith("type:"):
+                if type_line is not None:
+                    raise ValueError("Multiple type lines found")
+                type_line = line[5:].strip()
+            elif line.startswith("path:"):
+                if path_line is not None:
+                    raise ValueError("Multiple path lines found")
+                path_line = line[5:].strip()
+            elif line.startswith("```"):
+                has_content = True
+                record_content = not record_content
+            elif record_content:
+                content_lines.append(line)
+
+        if type_line is None:
+            raise ValueError("No type line found")
+        if len(content_lines) == 0 and has_content == False:
+            raise ValueError("No content found")
+
+        content = "\n".join(content_lines)
+
+        return type_line, path_line, content
+    
+    def encode(self, input_data: deque) -> str:
+        output = ""
+        for item in input_data:
+            output += f"type: {item['type']}\n"
+            if 'path' in item:
+                output += f"path: {item['path']}\n"
+            output += "```\n"
+            output += f"{item['content']}\n"
+            output += "```\n"
+        return output
+    
+    def close_open_backticks(self, string: str) -> str:
+        if string.count('\n```') % 2 != 0:
+            string += '\n```'
+        return string
+    
     def test(self):
         test_input = """type: command
 path: /workspace/
@@ -125,73 +194,4 @@ class _MyHomePageState extends State<MyHomePage> {
 #2. Make the web server accessible from outside the container: Configure the container to expose port 8080 and map it to the host machine."""
         parsed_data = self.decode(test_input)
         print(parsed_data)
-        print(self.encode(parsed_data))
-
-    def decode(self, input_string: str) -> deque:
-        data = input_string.strip().split("type:")[1:]
-        if len(data) == 0:
-            raise ValueError("No valid items found")
-        parsed_data = deque([])
-
-        for item in data:
-            item = "type:" + item.strip()
-            type, path, content = self._split_data(item)
-            dict = {}
-            dict["type"] = type
-            if path is not None:
-                dict["path"] = path
-            dict["content"] = content
-            parsed_data.append(dict)
-
-        if len(parsed_data) == 0:
-            raise ValueError("No valid items found")
-
-        return parsed_data
-    
-    def _split_data(self, input_data):
-        lines = input_data.split('\n')
-        type_line, path_line = None, None
-        content_lines = []
-        record_content = False
-        has_content = False
-
-        for line in lines:
-            #line = line.strip()
-            if line.startswith("type:"):
-                if type_line is not None:
-                    raise ValueError("Multiple type lines found")
-                type_line = line[5:].strip()
-            elif line.startswith("path:"):
-                if path_line is not None:
-                    raise ValueError("Multiple path lines found")
-                path_line = line[5:].strip()
-            elif line.startswith("```"):
-                has_content = True
-                record_content = not record_content
-            elif record_content:
-                content_lines.append(line)
-
-        if type_line is None:
-            raise ValueError("No type line found")
-        if len(content_lines) == 0 and has_content == False:
-            raise ValueError("No content found")
-
-        content = "\n".join(content_lines)
-
-        return type_line, path_line, content
-    
-    def encode(self, input_data: deque) -> str:
-        output = ""
-        for item in input_data:
-            output += f"type: {item['type']}\n"
-            if 'path' in item:
-                output += f"path: {item['path']}\n"
-            output += "```\n"
-            output += f"{item['content']}\n"
-            output += "```\n"
-        return output
-    
-    def close_open_backticks(self, string: str) -> str:
-        if string.count('\n```') % 2 != 0:
-            string += '\n```'
-        return string
+        print(self.encode(deque(parsed_data)))
